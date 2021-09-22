d8e3a6fdcb62fb807cb773f31e633fe7d7fc31b2
Stigma431 <86232552+Stigma431@users.noreply.github.com>
2021-09-22 13:40:51 +0500

    Обновление header

---
+++ js/header_main-menu.js
@@ -0,0 +1,8 @@
+let nav1 = document.getElementById('nav-link1')
+let nav2 = document.getElementById('nav-link2')
+let nav3 = document.getElementById('nav-link3')
+let nav4 = document.getElementById('nav-link4')
+let nav5 = document.getElementById('nav-link5')
+
+nav1.onmouseover = function(){nav2.style.color='gray'};
+nav1.onmouseout = function(){nav2.style.color=''};
