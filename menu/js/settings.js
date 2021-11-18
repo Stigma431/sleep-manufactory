df48bebc0181546d758b3b2f5e52afdf9b628cb1
Stigma431 <86232552+Stigma431@users.noreply.github.com>
2021-11-18 15:56:57 +0500

    menu

---
+++ menu/js/settings.js
@@ -0,0 +1,30 @@
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        const menu = new Mmenu("#menu", {
+            "extensions": [
+                "pagedim-black",
+                "position-front"
+            ],
+            "counters": false,
+            "navbars": [{
+                    "position": "top",
+                    "content": [
+                        "close",
+                        "<a href=''><img src='images/Vector.svg'></a>"
+                    ]
+                },
+                {
+                    "position": "bottom",
+                    "content": [
+                        "<a class='fa fa-envelope' href='#/'></a>",
+                        "<a class='fa fa-twitter' href='#/'></a>",
+                        "<a class='fa fa-facebook' href='#/'></a>"
+                    ]
+                },
+            ]
+
+        });
+
+    }
+
+);
