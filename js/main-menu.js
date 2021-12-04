da02d2efb39aa21b9556b26c777a063b4c69062f
Stigma431 <86232552+Stigma431@users.noreply.github.com>
2021-12-04 21:24:33 +0500

    Мелкие правки

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,12 +1,16 @@
 $(".mm-listitem__text-one").hover(function () {
-    $('.sub-menu_image-container-one').toggleClass("image-container-active");
+    $('.sub-menu_image-container-one').addClass("image-container-active");
+    $('.sub-menu_image-container-one').removeClass("image-container-desabled");
 });
 $(".mm-listitem__text-two").hover(function () {
-    $('.sub-menu_image-container-one').toggleClass("image-container-desabled");
-    $('.sub-menu_image-container-two').toggleClass("image-container-desabled");
+    $('.sub-menu_image-container-one').addClass("image-container-desabled");
+    $('.sub-menu_image-container-two').removeClass("image-container-desabled");
+    $('.sub-menu_image-container-two').addClass("image-container-active");
 });
 $(".mm-listitem__text-three").hover(function () {
-    $('.sub-menu_image-container-one').toggleClass("image-container-desabled");
-    $('.sub-menu_image-container-two').toggleClass("image-container-active");
-    $('.sub-menu_image-container-three').toggleClass("image-container-desabled");
+    $('.sub-menu_image-container-one').addClass("image-container-desabled");
+    $('.sub-menu_image-container-two').removeClass("image-container-active");
+    $('.sub-menu_image-container-two').addClass("image-container-desabled");
+    $('.sub-menu_image-container-three').removeClass("image-container-desabled");
+    $('.sub-menu_image-container-three').addClass("image-container-active");
 });
